7ce1f8609cff0f8b1c3ba1212f82431c25fc1897
h2190697689 <2190697689@qq.com>
2020-06-02 19:31:24 +0800

    first commit

---
+++ arr/arr.js
@@ -0,0 +1,11 @@
+/* 1 二维数组的查找 */
+function(arr,target){
+	const n = arr.length;
+	const m = arr[0].length;
+	let row = n - 1;
+	let col = 0;
+	if(m===0 && n === 0){
+		return false;
+	}
+	while(row > )
+}
---
+++ jest.js
@@ -0,0 +1,3 @@
+test("sum(2,2)不等于 5",()=>{
+	expect(sum(2,2).not.toBe(5))
+})

d6360db64f95d0db3129b24f5ede9025666b2213
h2190697689 <2190697689@qq.com>
2020-08-10 19:22:52 +0800

    all push

--- offer/3-printNode.js
+++ offer/3-printNode.js
@@ -2,3 +2,19 @@
  * @author hejiamin
  * @description 输入一个链表，从尾到头打印链表每个节点的值
  */
+
+ function Node(value){
+     this.value = value;
+     this.next = null
+ }
+
+
+ function printListFromTailToHead(head){
+     const res = [];
+     let presetNode = head;
+     while(presetNode.next){
+         res.unshift(presetNode.value);
+         presetNode = presetNode.next;
+     }
+     return res;
+ }
